d9222009ab33940ae0671e89a8172ad3ae3ccc5f
ics-kawakatsu <kentaro.kawakatsu@ics-web.jp>
2016-03-04 17:01:16 +0900

    160304_random_distribution : 乱数の種類と説明を追加

--- js/test.js
+++ js/test.js
@@ -7,37 +7,93 @@ var Main = (function () {
         this.init();
     }
     Main.prototype.init = function () {
+        var _this = this;
+        var selectBox = document.getElementById("selectBox");
+        //selectBox.addEventListener("select", ()=>function(event:Event):void{this.onSelect(event)})
         // Stageオブジェクトを作成します
         this.stage = new createjs.Stage("myCanvas");
-        console.log("test");
-        this.draw();
-        // Stageの描画を更新します
+        this.graphContainer = new createjs.Container();
+        this.stage.addChild(this.graphContainer);
+        this.randomFunc = this.random2;
+        this.countDistribution();
+        createjs.Ticker.addEventListener("tick", function () { _this.handleTick(); });
+    };
+    Main.prototype.handleTick = function () {
         this.stage.update();
     };
-    Main.prototype.draw = function () {
+    Main.prototype.onSelect = function (event) {
+        console.log(event);
+        switch (event) {
+            case 1:
+                this.randomFunc = this.random1;
+                break;
+            case 2:
+                this.randomFunc = this.random2;
+                break;
+            case 3:
+                this.randomFunc = this.random3;
+                break;
+            case 4:
+                this.randomFunc = this.random4;
+                break;
+            default:
+                break;
+        }
+        this.countDistribution();
+    };
+    Main.prototype.countDistribution = function () {
+        this.graphContainer.removeAllChildren();
         var values = [];
-        for (var i = 0; i < 100; i++) {
+        for (var i = 0; i < Main.DIVISION_NUMBER; i++) {
             values[i] = 0;
         }
-        for (var i = 0; i < 500; i++) {
-            var value = Math.random() * Math.random();
-            var tmp = Math.floor(value * 100);
-            values[tmp] += 1;
+        for (var i = 0; i < Main.SAMPLE_NUM; i++) {
+            var value = this.randomFunc();
+            var quantized = Math.floor(value * Main.DIVISION_NUMBER);
+            values[quantized] += 1;
         }
-        for (var i = 0; i < 100; i++) {
-            console.log(values[i]);
-            this.drawPoint(i * 5, values[i] * 10);
+        var maxCount = 0;
+        for (var i = 0; i < Main.DIVISION_NUMBER; i++) {
+            var count = values[i];
+            if (count > maxCount) {
+                maxCount = count;
+            }
+        }
+        var dy = Main.GRAPH_HEIGHT / maxCount;
+        for (var i = 0; i < Main.DIVISION_NUMBER; i++) {
+            this.drawPoint(i * 5, values[i] * dy);
         }
     };
     Main.prototype.drawPoint = function (posX, posY) {
-        //console.log(posX, posY);
         var shape = new createjs.Shape();
-        shape.graphics.beginFill((0x1000000 * Math.random()).toString());
+        shape.graphics.beginFill(createjs.Graphics.getHSL(posX / Main.GRAPH_WIDTH * 300, 90, 40));
         shape.graphics.drawCircle(0, 0, 5);
         shape.x = posX;
         shape.y = posY;
-        this.stage.addChild(shape); // 表示リストに追加
+        this.graphContainer.addChild(shape);
+        this.graphContainer.addChild(shape);
     };
+    //
+    Main.prototype.random1 = function () {
+        return Math.random();
+    };
+    Main.prototype.random2 = function () {
+        var r = Math.random();
+        return r * r;
+    };
+    Main.prototype.random3 = function () {
+        return Math.random() * Math.random();
+    };
+    Main.prototype.random4 = function () {
+        return Math.sqrt(Math.random());
+    };
+    Main.prototype.random5 = function () {
+        return (Math.random() + Math.random()) / 2;
+    };
+    Main.SAMPLE_NUM = 500;
+    Main.DIVISION_NUMBER = 100;
+    Main.GRAPH_WIDTH = 500;
+    Main.GRAPH_HEIGHT = 500;
     return Main;
 })();
 ;
